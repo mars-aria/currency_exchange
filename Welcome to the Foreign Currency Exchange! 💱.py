902f4bceb0e33951a987b29db12eb6519b22d6b5
Marshanah T. ♡ <marshanah.taylor@utexas.edu>
2023-03-16 15:29:26 -0500

    Update Welcome to the Foreign Currency Exchange! 💱.py

--- Welcome to the Foreign Currency Exchange! 💱.py
+++ Welcome to the Foreign Currency Exchange! 💱.py
@@ -4,33 +4,24 @@
 
 # In[1]:
 
-
 def to_USD(yen):
     return yen * 0.009082815
 
-
 # In[2]:
-
 
 to_USD(500)
 
-
 # In[3]:
 
-
 #*Note: 1 Japanese Yen = 0.0090670298 United States Dollar | 1 United States Dollar = 110.2897 Japanese Yen
-
 
 # This to_yen() function performs the opposite; it converts from U.S. Dollars ($) to Japanese yen (¥). *Reference: Same as above.
 
 # In[4]:
 
-
 def to_yen(USD):
     return USD * 110.2897
 
-
 # In[5]:
-
 
 to_yen(500)
